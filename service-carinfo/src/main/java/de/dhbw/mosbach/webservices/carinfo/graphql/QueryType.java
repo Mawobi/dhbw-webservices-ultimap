a97d70e40113bc744a2a5a80128955ed483294db
Kilian Krampf <kilian@krampf.de>
2021-02-12 23:07:15 +0100

    Register GraphQL-Query as Service-Bean

--- service-carinfo/src/main/java/de/dhbw/mosbach/webservices/carinfo/graphql/QueryType.java
+++ service-carinfo/src/main/java/de/dhbw/mosbach/webservices/carinfo/graphql/QueryType.java
@@ -1,10 +1,12 @@
 package de.dhbw.mosbach.webservices.carinfo.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class QueryType implements GraphQLQueryResolver {
     public CarInfoType getCarInfo (int carId) {
         return new CarInfoType();
--- service-routing/src/main/java/de/dhbw/mosbach/webservices/routing/graphql/QueryType.java
+++ service-routing/src/main/java/de/dhbw/mosbach/webservices/routing/graphql/QueryType.java
@@ -1,7 +1,9 @@
 package de.dhbw.mosbach.webservices.routing.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Service;
 
+@Service
 public class QueryType implements GraphQLQueryResolver {
     public CoordinateType geocode (String name) {
         return new CoordinateType(9.773, 49.490);

763afeff85a0d10c3eb5ef868ea97ddf1b27d0eb
Kilian Krampf <kilian@krampf.de>
2021-02-12 23:54:30 +0100

    Create service-ultimap and define graphql-schemes

---
+++ service-ultimap/src/main/java/de/dhbw/mosbach/webservices/ultimap/ServiceUltimapApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.mosbach.webservices.ultimap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUltimapApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(ServiceUltimapApplication.class, args);
+    }
+
+}

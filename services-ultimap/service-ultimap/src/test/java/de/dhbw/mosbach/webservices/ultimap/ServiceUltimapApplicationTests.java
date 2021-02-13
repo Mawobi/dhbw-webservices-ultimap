0553d12e0683f00ef2f1a879e9870de474c71fd6
Kilian Krampf <kilian@krampf.de>
2021-02-13 19:59:52 +0100

    Fix failing test

--- services-ultimap/service-ultimap/src/test/java/de/dhbw/mosbach/webservices/ultimap/ServiceUltimapApplicationTests.java
+++ services-ultimap/service-ultimap/src/test/java/de/dhbw/mosbach/webservices/ultimap/ServiceUltimapApplicationTests.java
@@ -1,4 +1,4 @@
-package de.dhbw.mosbach.webservices.routing;
+package de.dhbw.mosbach.webservices.ultimap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

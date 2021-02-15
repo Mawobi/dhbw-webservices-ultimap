f61417c1cd22e03e524f267dcefd79be56cd8777
Kilian Krampf <kilian@krampf.de>
2021-02-15 20:57:24 +0100

    Implement GraphQL Endpoints

--- services-ultimap/service-carinfo/src/main/java/de/dhbw/mosbach/webservices/carinfo/graphql/CarInfoDataFetcher.java
+++ services-ultimap/service-carinfo/src/main/java/de/dhbw/mosbach/webservices/carinfo/graphql/CarInfoDataFetcher.java
@@ -3,37 +3,42 @@ package de.dhbw.mosbach.webservices.carinfo.graphql;
 
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
+import de.dhbw.mosbach.webservices.carinfo.data.Car;
+import de.dhbw.mosbach.webservices.carinfo.data.ICarRepository;
+import de.dhbw.mosbach.webservices.carinfo.external.IFuelPriceProvider;
 import de.dhbw.mosbach.webservices.ultimap.graphql.types.CarInfoType;
 import de.dhbw.mosbach.webservices.ultimap.graphql.types.FuelPriceType;
 import de.dhbw.mosbach.webservices.ultimap.graphql.types.FuelType;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @DgsComponent
 public class CarInfoDataFetcher {
+    private final ICarRepository carRepository;
+    private final IFuelPriceProvider fuelPriceProvider;
+
+    public CarInfoDataFetcher (ICarRepository carRepository, IFuelPriceProvider fuelPriceProvider) {
+        this.carRepository = carRepository;
+        this.fuelPriceProvider = fuelPriceProvider;
+    }
+
     @DgsData(parentType = "Query", field = "car")
     public CarInfoType getCar (int carId) {
-        return new CarInfoType(
-                0,
-                "Audi A3",
-                6.9,
-                FuelType.BENZOL
-        );
+        return carRepository.findById(carId).orElseThrow().toCarInfoType();
     }
 
     @DgsData(parentType = "Query", field = "fuel")
     public FuelPriceType getFuel (FuelType typ) {
-        return new FuelPriceType(1.449);
+        return new FuelPriceType(fuelPriceProvider.getFuelPrice(typ));
     }
 
     @DgsData(parentType = "Query", field = "allCars")
     public List<CarInfoType> getAllCars () {
-        return Collections.singletonList(new CarInfoType(
-                0,
-                "Audi A3",
-                6.9,
-                FuelType.BENZOL
-        ));
+        return StreamSupport
+                .stream(carRepository.findAll().spliterator(), false)
+                .map(Car::toCarInfoType)
+                .collect(Collectors.toList());
     }
 }

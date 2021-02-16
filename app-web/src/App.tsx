b9b2feba17179dad214ac61f1439568f11083c92
Lars Rickert <lars.rickert@web.de>
2021-02-16 17:43:47 +0100

    added basic structure

--- app-web/src/App.tsx
+++ app-web/src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import './App.scss';
+import React, { useEffect } from 'react';
+import './styles/App.scss';
+import Header from './components/Header/Header';
 
-function App() {
-  return <p> Hallo Welt!</p>;
+export default function App() {
+  // disable preload
+  useEffect(() => {
+    document.body.classList.remove('preload');
+  });
+
+  return (
+    <div>
+      <Header />
+      <p> Hallo Welt!</p>
+      <button className={'btn'} onClick={toggleDarkMode}>
+        Theme ändern
+      </button>
+    </div>
+  );
 }
 
-export default App;
+function toggleDarkMode() {
+  const html = document.documentElement;
+  html.setAttribute('theme', html.getAttribute('theme') === 'dark' ? 'light' : 'dark');
+}
---
+++ app-web/src/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header() {
+  return <header className={'um-header'}>Ich bin ein Header</header>;
+}

2727482fda728b7e0a1323af82a3ea76528948d3
Kilian Krampf <kilian@krampf.de>
2021-02-23 13:47:20 +0100

    Fix bootJar not working for spring-data-jpa (possibly because of something with hibernate)

--- services-ultimap/service-carinfo/build.gradle.kts
+++ services-ultimap/service-carinfo/build.gradle.kts
@@ -10,6 +10,9 @@ version = "0.0.1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    //This is needed to be able to package the service to jar. For some strange reason it is not included by default.
+    implementation("net.minidev:json-smart:2.3")
+
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 
     testRuntimeOnly("com.h2database:h2")
